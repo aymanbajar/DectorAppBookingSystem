012264ada739205064021b40258e97afcd5b1540
Eymen Bacar <148905870+aymanbajar@users.noreply.github.com>
2025-11-25 23:00:08 +0300

    fix: correct import case for SpecialityMenu

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Banner from "../components/Banner.jsx";
 import Header from "../components/Header.jsx";
-import SpecialityMenu from "../components/specialityMenu.jsx";
+import SpecialityMenu from "../components/SpecialityMenu.jsx";
 import TopDoctors from "../components/TopDoctors.jsx";
 
 export default function Home(){
